cc75374b1d959374770dd080b046782c1d38ad8e
Carl Egbert and Matt Wilkin <carl@>
2016-06-08 08:52:17 -0700

    returned true when divisible y 4.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
     event.preventDefault();
     var yearInput = $("#yearInput").val();
     console.log(yearInput);
+    if (yearInput % 4 === 0) {
+      $("#positive").show();
+    }
     if (!(yearInput % 4 === 0)) {
       $("#negative").show();
     }
