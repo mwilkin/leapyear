e52d32d901da629d6bda831ad246e6463168d4c6
Carl Egbert and Matt Wilkin <matt@>
2016-06-08 09:00:08 -0700

    return false when year is divis. by 100

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,17 @@
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
-    var yearInput = $("#yearInput").val();
+    var yearInput = parseInt($("#yearInput").val());
     console.log(yearInput);
-    if (yearInput % 4 === 0) {
+
+
+    if (yearInput%100===0) {
+      $("#negative").show();
+    }
+    else if (yearInput % 4 === 0) {
       $("#positive").show();
     }
-    if (!(yearInput % 4 === 0)) {
+    else if (!(yearInput % 4 === 0)) {
       $("#negative").show();
     }
   })
